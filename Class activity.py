c595bb7e043832ef81c4a8ebdbe7de14c8fe155a
AdeelAhmad3 <ahmadadeell890@gmail.com>
2025-09-11 22:34:55 +0500

    Add files via upload

---
+++ Class activity.py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=int(input("Enter Your Number"))
+if a>=85:
+    print("a")
+elif a>=70:
+    print("B")
+elif a>=50:
+    print("C")
+else:
+    print("Fail")
